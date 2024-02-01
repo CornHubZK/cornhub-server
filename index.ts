cb39423d610c7aa278ee8d6c1cbcf01e73d11a7b
Theo Madzou <madztheo@hotmail.fr>
2024-02-01 19:44:36 +0000

    naming changes

--- index.ts
+++ index.ts
@@ -1,18 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
-import countries from "i18n-iso-countries";
 import type { Alpha3Code } from "i18n-iso-countries";
 import { randomUUID } from "crypto";
 const app = express();
 
-type Request = {
+type ProofRequest = {
   id: string;
   minAge: number;
   citizenship: Alpha3Code;
   status: "pending" | "successful" | "unsuccessful";
 };
 
-const requests: { [key: string]: Request } = {};
+const proofRequests: { [key: string]: ProofRequest } = {};
 
 const PORT = process.env.PORT || 3010;
 
@@ -21,37 +20,37 @@ app.use(bodyParser.json());
 app.post("/request/create", (req, res) => {
   const { minAge, citizenship } = req.body;
   const id = randomUUID();
-  const request: Request = {
+  const proofRequest: ProofRequest = {
     id,
     minAge,
     citizenship,
     status: "pending",
   };
-  requests[id] = request;
-  res.send(request);
+  proofRequests[id] = proofRequest;
+  res.send(proofRequest);
 });
 
 app.get("/request/:id", (req, res) => {
   const { id } = req.params;
-  if (!requests[id]) {
+  if (!proofRequests[id]) {
     res.status(404).send("Request not found");
     return;
   }
-  res.send(requests[id]);
+  res.send(proofRequests[id]);
 });
 
 app.post("/request/complete", (req, res) => {
   const { isValid, requestId } = req.body;
-  if (!requests[requestId]) {
+  if (!proofRequests[requestId]) {
     res.status(404).send("Request not found");
     return;
   }
-  if (requests[requestId].status !== "pending") {
+  if (proofRequests[requestId].status !== "pending") {
     res.status(400).send("Request already completed");
     return;
   }
-  requests[requestId].status = isValid ? "successful" : "unsuccessful";
-  res.send(requests[requestId]);
+  proofRequests[requestId].status = isValid ? "successful" : "unsuccessful";
+  res.send(proofRequests[requestId]);
 });
 
 app.listen(PORT, () => {
